54cd220ca5861bb7c2654e9913a69852a2ece748
AlanaVitoria <126632009+AlanaVitoria@users.noreply.github.com>
2024-03-25 08:01:39 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,43 @@
+def cadastrar_dados():
+    nome = input("Digite o nome: ")
+    endereco = input("Digite o endereço: ")
+    with open ("dados.txt", "a") as arquivo:
+        arquivo.write(f"{nome};{endereco}\n")
+    print("Dados cadastrados com sucesso!")
+
+
+def ler_dados():
+    try:
+        with open("dados.txt", "r") as arquivo:
+            print("Lista de dados cadastrados:")
+            for linha in arquivo
+                dados = linha.strip().split(';')
+                if len(dados) = 2:
+                    nome, endereco = dados
+                    print(f"Nome: {nome}, Endereço: {endereco}")
+                else:
+                    print("Erro ao ler linha de arquivos", linha)
+    except FileNotFoundError:
+        print("Nenhum dado cadastrado ainda.")
+
+def main():
+    while True:
+        print("\n===== Menu de Opções =====")
+        print("1. Cadastrar dados")
+        print("2. Ler dados cadastrados")
+        print("3. Sair")
+
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == "1":
+            cadastrar_dados()
+        elif opcao == "2":
+            ler_dados()
+        elif opcao == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+            
+if __name__ == "__main__":
+    main()
